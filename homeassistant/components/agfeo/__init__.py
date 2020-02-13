fc2f431c138937ec4c84371df179ffda481b5054
Ralf Weinbrecher <rweinbrecher@agfeo.de>
2020-02-13 12:00:19 +0000

    Reading configuration for Agfeo integration during initialization

--- homeassistant/components/agfeo/__init__.py
+++ homeassistant/components/agfeo/__init__.py
@@ -1,50 +1,47 @@
-"""The AGFEO SmartHome integration."""
-import asyncio
+"""Support for Agfeo SmartHome interface."""
+import logging
 
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+import homeassistant.helpers.config_validation as cv
 
-from .const import DOMAIN
+_LOGGER = logging.getLogger(__name__)
 
-CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
+DOMAIN = "agfeo"
 
-# TODO List the platforms that you want to support.
-# For your initial PR, limit it to 1 platform.
-PLATFORMS = ["light"]
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_HOST): cv.string,
+                vol.Required(CONF_USERNAME): cv.string,
+                vol.Required(CONF_PASSWORD): cv.string,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
 
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the AGFEO SmartHome component."""
-    hass.states.async_set("agfeo_state.test", "ok")
+def setup(hass, config):
+    """Set up for the Asterisk Voicemail box."""
+    conf = config.get(DOMAIN)
+
+    host = conf.get(CONF_HOST)
+    username = conf.get(CONF_USERNAME)
+    password = conf.get(CONF_PASSWORD)
+
+    hass.data[DOMAIN] = AgfeoData(hass, host, username, password, config)
+
     return True
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up AGFEO SmartHome from a config entry."""
-    # TODO Store an API object for your platforms to access
-    # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
+class AgfeoData:
+    """Store Agfeo SmartHome data."""
 
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    def __init__(self, hass, host, username, password, config):
+        """Init the Agfeo SmartHome data object."""
 
-    return True
-
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, component)
-                for component in PLATFORMS
-            ]
-        )
-    )
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
+        self.hass = hass
+        self.config = config
